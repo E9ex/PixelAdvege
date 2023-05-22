0fdee51af769ca5b15999936e9c0f87bfebf6ef5
E9ex <egecansar@gmail.com>
2023-05-23 00:07:39 +0300

    jumpanimdone

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -6,39 +6,44 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private float speed= 5f;
+    private float speed = 5f;
     private Animator playerAnim;
+    private foot _foot;
+    public bool isjumpin = false;
+   
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         playerAnim = GetComponent<Animator>();
+        _foot = GameObject.FindObjectOfType<foot>();
     }
 
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void FixedUpdate()
     {
         float h = Input.GetAxis("Horizontal");
+      //  float y = Input.GetAxis("Vertical");
         move(h);
         playerturn(h);
+        PlayerAnim(h);
+        //JumpAnim();
+
     }
 
     void move(float h)
     {
         rb.velocity = new Vector2(h * speed, rb.velocity.y);
-        playerAnim.SetBool("kosuyorMu",true);
-        if (h==0)
-            playerAnim.SetBool("kosuyorMu",false);
     }
 
     void playerturn(float h)
@@ -52,4 +57,31 @@ public class Player : MonoBehaviour
             transform.localScale = new Vector3(1, 1, 1);
         }
     }
+
+    public void PlayerAnim(float h)
+    {
+        playerAnim.SetBool("kosuyorMu", true);
+        if (h == 0)
+        {
+            playerAnim.SetBool("kosuyorMu", false);
+        }
+        else if (rb.velocity.y<-.1f)
+        {
+            playerAnim.SetBool("zipliyormU",false);
+        }
+    }
+
+   /* public void JumpAnim()
+    {
+        if (_foot.transform.position.y == 0&&!isjumpin)
+        {
+            isjumpin = true;
+            playerAnim.SetBool("zipliyormU",true);
+        }
+        else if(_foot.yerdeMi)
+        {
+            isjumpin = false;
+            playerAnim.SetBool("zipliyormU",false);
+        }
+    }*/
 }
--- Assets/scripts/foot.cs
+++ Assets/scripts/foot.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,14 @@ using UnityEngine;
 public class foot : MonoBehaviour
 {
     [SerializeField]  LayerMask layer;// bu değişken 'Raycast' işlemi için kullanılan layer'ı belirler.
-    [SerializeField] bool yerdeMi = true; // bu değişken zemine dokunulup dokunulmadığını kontrol eder.
+   public  bool yerdeMi = true; // bu değişken zemine dokunulup dokunulmadığını kontrol eder.
     [SerializeField] Rigidbody2D rb;
     public float jumpspeed = 10f;
+    [SerializeField] Animator playeraanim;
+    private bool hasJumped = false;
+    
+
+
     void Update()
     {
         RaycastHit2D iscollider = Physics2D.Raycast(transform.position, Vector2.down, 0.25f, layer);
@@ -16,11 +22,18 @@ public class foot : MonoBehaviour
             yerdeMi = true;
         }
         else
+        {
             yerdeMi = false;
-        
+        }
         if (yerdeMi&&Input.GetKeyDown(KeyCode.Space)&&rb.velocity.y==0)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpspeed);
+           playeraanim.SetBool("zipliyormU",true);
         }
+        /*else if (rb.velocity.y<-1f)
+        {
+            playeraanim.SetBool("zipliyormU",false);
+        }*/
+      
     }
 }
