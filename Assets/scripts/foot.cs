574a05c36d048152ddcebfadb07f303e57e28ffc
E9ex <egecansar@gmail.com>
2023-05-22 21:39:31 +0300

    ziplio:D sonunda

--- Assets/scripts/foot.cs
+++ Assets/scripts/foot.cs
@@ -5,21 +5,20 @@ using UnityEngine;
 public class foot : MonoBehaviour
 {
     [SerializeField]  LayerMask layer;// bu değişken 'Raycast' işlemi için kullanılan layer'ı belirler.
-    public  bool IsGround = true;// bu değişken zemine dokunulup dokunulmadığını kontrol eder.
+    [SerializeField] bool yerdeMi = true; // bu değişken zemine dokunulup dokunulmadığını kontrol eder.
     [SerializeField] Rigidbody2D rb;
     public float jumpspeed = 10f;
     void Update()
     {
-        RaycastHit2D iscollider = Physics2D.Raycast(transform.position, Vector2.down, 0.09f, layer);
-        if (iscollider!=null)
+        RaycastHit2D iscollider = Physics2D.Raycast(transform.position, Vector2.down, 0.25f, layer);
+        if (iscollider != null)
         {
-            IsGround = true;
+            yerdeMi = true;
         }
         else
-        {
-            IsGround = false;
-        }
-        if (IsGround&&Input.GetKeyDown(KeyCode.Space)&&rb.velocity.y==0)
+            yerdeMi = false;
+        
+        if (yerdeMi&&Input.GetKeyDown(KeyCode.Space)&&rb.velocity.y==0)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpspeed);
         }
